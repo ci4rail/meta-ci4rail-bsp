fc37885fc910db9e6ab02ecd2150314bd66f9351
KlausPopp <klauspopp@gmx.de>
2025-04-24 10:36:24 +0200

    usb net: give each dnsmasq its own port

--- recipes-core/udev/files/usb-netif-setup.sh
+++ recipes-core/udev/files/usb-netif-setup.sh
@@ -9,38 +9,47 @@ case "$IFACE" in
   usb_io_ctrl)
     IP="192.168.200.10"
     RANGE="192.168.200.1,192.168.200.1"
+    DNSMASQ_PORT="20001"
     ;;
   usb_ext1)
     IP="192.168.201.10"
     RANGE="192.168.201.1,192.168.201.1"
+    DNSMASQ_PORT="20002"
     ;;
   usb_ext2)
     IP="192.168.202.10"
     RANGE="192.168.202.1,192.168.202.1"
+    DNSMASQ_PORT="20002"
     ;;
   usb_ext3)
     IP="192.168.203.10"
     RANGE="192.168.203.1,192.168.203.1"
+    DNSMASQ_PORT="20003"
     ;;
   usb_ext4)
     IP="192.168.204.10"
     RANGE="192.168.204.1,192.168.204.1"
+    DNSMASQ_PORT="20004"
     ;;
   usb_ext5)
     IP="192.168.205.10"
     RANGE="192.168.205.1,192.168.205.1"
+    DNSMASQ_PORT="20005"
     ;;
   usb_ext6)
     IP="192.168.206.10"
     RANGE="192.168.206.1,192.168.206.1"
+    DNSMASQ_PORT="20006"
     ;;
   usb_ext7)
     IP="192.168.207.10"
     RANGE="192.168.207.1,192.168.207.1"
+    DNSMASQ_PORT="20007"
     ;;
   usb_ext8)
     IP="192.168.208.10"
     RANGE="192.168.208.1,192.168.208.1"
+    DNSMASQ_PORT="20008"
     ;;
   *)
     echo "Unknown interface: $IFACE" >&2
@@ -72,5 +81,5 @@ systemd-run --unit=dnsmasq-$IFACE \
   --dhcp-range="$RANGE",12h \
   --pid-file="$PIDFILE" \
   --dhcp-leasefile=/var/lib/misc/dnsmasq-$IFACE.leases \
-  --port=5353
+  --port="$DNSMASQ_PORT"
 
